5af111641ffe3666ac9406672830ef43b9e529b8
Goswami <vidit.goswami@metrosystems.net>
2018-03-14 13:37:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
 
 class App extends Component {
   render() {
@@ -9,9 +10,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <h2>Hello!</h2>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <br/>Here goes your code!
         </p>
       </div>
     );
