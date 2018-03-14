160b60371fbdbe9248f4413e09062716f4c3b0b2
Goswami <vidit.goswami@metrosystems.net>
2018-03-14 14:09:26 +0530

    hello added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
           <h2>Hello!</h2>
+		  <h2>Hello!</h2>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
